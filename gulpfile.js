677554610a8faa91645dc03dc0c7723a3051a7b8
Alan <thatguypotter@gmail.com>
2017-12-04 23:16:13 -0500

    daily commit after tutorial

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars');
+
+gulp.task('default', function() {
+  console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+  	.pipe(postcss([cssvars, autoprefixer]))
+  	.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+
+});
